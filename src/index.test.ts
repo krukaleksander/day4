e7b27c8e3a467f674f791e09bab935696531cee6
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-24 09:40:47 +0100

    test endpoints for current mid EUR/PLN USD/PLN

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,57 +1,30 @@
 import fetch from "node-fetch";
 
-test("works", async () => {
-  const response = await fetch("https://api.github.com/users/github");
-  const data = (await response.json()) as IGithubUser;
+test("Eur/PLN", async () => {
+  const response = await fetch(
+    "http://api.nbp.pl/api/exchangerates/rates/a/eur/"
+  );
+  const data: ICurrencyResponse = await response.json();
 
   console.log(data);
 });
 
-const response: IGithubUser = {
-  login: "github",
-  id: 9919,
-  node_id: "MDEyOk9yZ2FuaXphdGlvbjk5MTk=",
-  avatar_url: "https://avatars.githubusercontent.com/u/9919?v=4",
-  gravatar_id: "",
-  type: "Organization",
-  site_admin: false,
-  name: "GitHub",
-  company: null,
-  blog: "https://github.com/about",
-  location: "San Francisco, CA",
-  email: null,
-  hireable: null,
-  bio: "How people build software.",
-  twitter_username: null,
-  public_repos: 409,
-  public_gists: 0,
-  followers: 0,
-  following: 0,
-  created_at: "2008-05-11T04:37:31Z",
-  updated_at: "2020-09-28T06:15:10Z",
-};
+test("USD/PLN", async () => {
+  const response = await fetch(
+    "http://api.nbp.pl/api/exchangerates/rates/a/usd/"
+  );
+  const data: ICurrencyResponse = await response.json();
 
-interface IGithubUser {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: "";
-  type: "Organization";
-  site_admin: false;
-  name: "GitHub";
-  company: null;
-  blog: "https://github.com/about";
-  location: "San Francisco, CA";
-  email: null;
-  hireable: null;
-  bio: "How people build software.";
-  twitter_username: null;
-  public_repos: 409;
-  public_gists: 0;
-  followers: 0;
-  following: 0;
-  created_at: "2008-05-11T04:37:31Z";
-  updated_at: "2020-09-28T06:15:10Z";
-  // ...
+  console.log(data);
+});
+
+interface ICurrencyResponse {
+  table: string;
+  currency: string;
+  code: string;
+  rates: {
+    no: string;
+    effectiveDate: string;
+    mid: number;
+  }[];
 }
