25970858cc7ebd47794eac9f428dd4743ce89595
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-24 10:02:17 +0100

    test difference between today EUR/PLN rate and month before

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-test("Eur/PLN", async () => {
+test.skip("Eur/PLN", async () => {
   const response = await fetch(
     "http://api.nbp.pl/api/exchangerates/rates/a/eur/"
   );
@@ -8,12 +8,35 @@ test("Eur/PLN", async () => {
   console.log(data);
 });
 
-test("USD/PLN", async () => {
+test.skip("USD/PLN", async () => {
   const response = await fetch(
     "http://api.nbp.pl/api/exchangerates/rates/a/usd/"
   );
   const data: ICurrencyResponse = await response.json();
   console.log(data);
+});
+
+test("difference between today EUR/PLN rate and month before", async () => {
+  const responseToday = await fetch(
+    "http://api.nbp.pl/api/exchangerates/rates/a/eur/2022-03-23/"
+  );
+  const dataToday: ICurrencyResponse = await responseToday.json();
+
+  console.log(dataToday);
+
+  const responseMonthAgo = await fetch(
+    "http://api.nbp.pl/api/exchangerates/rates/a/gbp/2022-02-23/"
+  );
+  const dataMonthAgo: ICurrencyResponse = await responseMonthAgo.json();
+
+  console.log(dataMonthAgo);
+
+  const difference: IDifferenceInRates = {
+    todayRate: dataToday.rates[0].mid,
+    agoRate: dataMonthAgo.rates[0].mid,
+    difference: dataToday.rates[0].mid - dataMonthAgo.rates[0].mid,
+  };
+  console.log(difference);
 });
 
 interface ICurrencyResponse {
@@ -27,4 +50,10 @@ interface IRatesArray {
   no: string;
   effectiveDate: string;
   mid: number;
+}
+
+interface IDifferenceInRates {
+  todayRate: number;
+  agoRate: number;
+  difference: number;
 }
